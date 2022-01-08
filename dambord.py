0ec71fdc5e990b6043cdbba984a433b22f42e4ea
arsKalishoek <99069312@mydavinci.nl>
2022-01-08 20:27:50 +0100

    ik heb een dambord gemaakt

---
+++ dambord.py
@@ -0,0 +1,25 @@
+import tkinter
+window = tkinter.Tk()
+window.title("Dambord")
+window.geometry("940x940")
+frame = tkinter.Frame(window)
+z=1
+for x in range(10):
+    if z==0:
+        z=1
+    else:
+        z=0
+    for y in range(10):
+        if z == 0:
+            b="black"
+            z=1
+        else:
+            b="white"
+            z=0
+        label = tkinter.Label(frame,
+        padx=40.6,
+        pady=30.7,
+        bg=b
+        ).grid(row=y,column=x)
+frame.pack(expand=True)
+window.mainloop()
